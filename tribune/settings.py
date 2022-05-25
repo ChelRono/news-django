ff0dc9130d5ecddca3154e009600cf6db8b7ea39
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-05-25 11:42:23 -0400

    deployment

--- tribune/settings.py
+++ tribune/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 's$%9$d0*)1xykvy)qvp2by9s48dq((966@aeb6%)!1w@0h3pwm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 MODE=config("MODE", default="dev")
